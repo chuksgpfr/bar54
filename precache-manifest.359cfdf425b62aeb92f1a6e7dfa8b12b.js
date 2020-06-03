e5c0ccaae9d7d6538ab8d13dc4a605702a79a8ff
Chukwudi Ubah <chuksgpfr@gmail.com>
2020-06-03 11:44:51 +0100

    Updates

---
+++ precache-manifest.359cfdf425b62aeb92f1a6e7dfa8b12b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4489a1bb2e3accb49a45c64b0daf9d0",
+    "url": "/bar54/index.html"
+  },
+  {
+    "revision": "43012459c09e00435d17",
+    "url": "/bar54/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "14522694d8a69ef97871",
+    "url": "/bar54/static/css/main.59c5d97a.chunk.css"
+  },
+  {
+    "revision": "43012459c09e00435d17",
+    "url": "/bar54/static/js/2.1f361d05.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/bar54/static/js/2.1f361d05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14522694d8a69ef97871",
+    "url": "/bar54/static/js/main.38bdb28b.chunk.js"
+  },
+  {
+    "revision": "a8eb581590a5acca9a57",
+    "url": "/bar54/static/js/runtime-main.0b0b2ae5.js"
+  },
+  {
+    "revision": "af836cf64846b501353ee0f933e367f3",
+    "url": "/bar54/static/media/post-1.af836cf6.jpg"
+  },
+  {
+    "revision": "3a5a0f6275e6db59d1d2f5e0118dad49",
+    "url": "/bar54/static/media/profile-bg.3a5a0f62.jpg"
+  },
+  {
+    "revision": "11e242c5d1710bef0426ff1e58a15e81",
+    "url": "/bar54/static/media/profile-image-1.11e242c5.png"
+  },
+  {
+    "revision": "b1bbec499a0d66e5403480e8cda1bcbe",
+    "url": "/bar54/static/media/profile-image-2.b1bbec49.png"
+  },
+  {
+    "revision": "4bca1396ce146324eaa6baee7391ebdd",
+    "url": "/bar54/static/media/undraw_Beer_celebration_cefj.4bca1396.svg"
+  }
+]);
